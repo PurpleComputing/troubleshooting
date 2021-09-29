0a5b0d458f5a5802a946a6233f5b4453a23d5e8a
Michael <30936296+mtan93@users.noreply.github.com>
2021-09-29 11:55:08 +0100

    Add files via upload

---
+++ remove-1password.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#####################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#   ScanSnap Manager Remover.sh -- Removes ScanSnap Manager
+#
+# SYNOPSIS
+#   sudo 1Password 7 Remover.sh
+#
+####################################################################################################
+#
+# HISTORY
+#
+#   Version: 1.0
+#
+#   - Martyn Watts, 13.08.2021 Initial Build
+#   - Michael Tanner, 29.09.2021 Modified Build
+#
+####################################################################################################
+# Script to remove 1Password 7.
+
+appName='1Password 7'
+#appFolder='ScanSnap/Sub'
+logfile="/Library/Logs/1PasswordRemoverScript.log"
+deplog="/var/tmp/depnotify.log"
+
+echo "Status: Installing ${appName}" >> ${deplog}
+
+# Check to see if the App is installed.
+
+    if [[ -e "/Applications/${appName}.app" ]]; then
+		/bin/echo "`date`: Uninstalling 1Password Manager" >> ${logfile}
+		/bin/echo "`date`: Uninstalling 1Password Manager"
+		killall 1Password\ 7
+		CurrentUser=$(/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }')
+		rm -rf /Applications/1Password\ 7.app
+
+    else
+		/bin/echo "`date`: 1Password not installed - Skipping" >> ${logfile}
+		/bin/echo "`date`: 1Password not installed - Skipping"
+    fi
+#jamf policy -id 036 >> ${logfile} - Reinstall using Install 1Password Policy
+/bin/echo "--" >> ${logfile}
+/bin/echo "Command: DeterminateManualStep: 1" >> ${deplog}
